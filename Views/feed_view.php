f29321546b3dd0cf64b5f840375cc513460b110e
jalil3004 <jalil.mzili@epitech.eu>
2023-03-19 15:51:58 +0100

    test feed

--- Views/feed_view.php
+++ Views/feed_view.php
@@ -12,75 +12,105 @@ include '../Controllers/feed_controller.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Twist Feed</title>
-    <link rel="stylesheet" href="./signin.css">
-    <style>
-        form {
-            margin-left: 500px;
-        }
-
-        input {
-            margin-left: 100px;
-        }
-    </style>
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://kit.fontawesome.com/b87d756cc2.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../Views/signin.css">
 </head>
-
-<body>
-    <div class="container">
-        <a href="../Views/account_view.php?pseudo=" .<?= $_SESSION['NICKNAME'] ?>>
-            <button style='margin-left:700px; height:50px; width:70px;' type="submit" id="account__button">Profile</button>
-        </a>
-        
-        <form id="tweet__form" method="get">
-            <label>Explorer</label>
-            <input type="text" id="tweet__field" name="message" placeholder="Post a new tweet !">
-            <button type="submit" id="tweet__button">Post</button><br>
-            <br>
-            
-        </form>    
-        <form method="post" action="../Views/feed_view.php" enctype="multipart/form-data">
-                        <input type="file" name="images">
-
-                        <input type="submit" value="Upload">
-                    </form>    
-            
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-    <?php
-
-
-require_once('../Controllers/like_controller.php');
-
-
-
-?>            
-
-
-            
-<div class="searchTag">
-    <form method="get">
-        <input type="text" id="tweet__field" name="searchTag" placeholder="Search a tag">
-        <button type="submit" name="search_tag">Search</button>
-        <?php require '../Models/Tags.php' ?>
-    </form>
+    <body class="bg-[#FAECBC]">
     
-</div>
-</div>
+      <nav class="bg-black border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+        <div class="container flex flex-wrap items-center justify-between mx-auto">
+          <a href="https://flowbite.com/" class="flex items-center">
+              <!-- <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" /> -->
+              <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+          </a>
+          <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+          </button>
+          <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-black ">
+              <li>
+                <a href="#" class="block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0 dark:text-white" aria-current="page">Home</a>
+              </li>
+              <li>
+                <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+              </li>
+              <li>
+                <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+              </li>
+              <li>
+                <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+              </li>
+              <li>
+                <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+              </li>
+              <li>
+                <a href="#"><i class="fa-solid fa-power-off text-white"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-<?php require_once('../Models/Photo.php');    ?>    
+      <form class ="justify-center flex " id="tweet__form" method="get">
+        <div class="w-[600px] mt-5 mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+            <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+                <label for="comment" class="sr-only">Your comment</label>
+                <textarea id="tweet_field" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 " placeholder="Start to twist..." required></textarea>
+            </div>
+            <div class="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
+                <button type="submit" id="tweet__button" class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-[#FFBF84]  rounded-lg hover:bg-orange">
+                    TWIST !
+                </button>
+                <div class="flex pl-0 space-x-1 sm:pl-2">
+                 
+                    <button type="button" class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                        <i class="fa-solid fa-image"></i>
+                        <span class="sr-only">Upload image</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+     </form>
+
+     <div class="container">
+        <div class ="row">
+            <div class="post">
+                
+            </div>
+        </div>
+     </div>
+     <?php
+
+
+     require_once('../Controllers/like_controller.php');
+     
+     
+     
+     ?>            
+     
+     
+                 
+     <div class="searchTag">
+         <form method="get">
+             <input type="text" id="tweet__field" name="searchTag" placeholder="Search a tag">
+             <button type="submit" name="search_tag">Search</button>
+             <?php require '../Models/Tags.php' ?>
+         </form>
+         
+     </div>
+     </div>
+     
+     <?php require_once('../Models/Photo.php');    ?>    
+     
+     <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+     <script src="../Ajax/tweet_post_ajax.js"></script>
+       
 </body>
-<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-<script src="../Ajax/tweet_post_ajax.js"></script>
-<!-- <script src="../Ajax/like_ajax.js"></script> -->
-
 </html>
